8d3f0c68e6a0234a5287a9fb37eac96957e4bf29
Edward Vidal Jr <develone@sbcglobal.net>
2025-07-15 20:02:52 -0600

    ice_makefile_uart_echo & ice_makefile_blinky

---
+++ examples/ice_makefile_uart_echo_blinky/top.v
@@ -0,0 +1,22 @@
+module top (
+    input CLK,
+    input UART_RX,
+    output UART_TX,
+    output LED_R,
+    output LED_G,
+    output LED_B
+);
+    assign UART_TX = UART_RX;
+      localparam N = 22;
+
+  reg [N:0] counter;
+
+  always @(posedge CLK) begin
+    counter <= counter + 1;
+  end
+
+  assign LED_R = 1'b1;
+  assign LED_G = counter[N];
+  assign LED_B = 1'b1;
+
+endmodule
